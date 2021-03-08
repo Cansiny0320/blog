cb71243d97003e4aa5e92fa98c4843ee82123ea9
Cansiny0320 <1136843910@qq.com>
2021-03-08 12:44:36 +0800

    feat: derfer script

--- src/plugin/plugin-baidu-analytics/index.js
+++ src/plugin/plugin-baidu-analytics/index.js
@@ -1,30 +1,31 @@
 module.exports = function (context, options) {
   return {
-    name: "docusaurus-baidu-analytics-plugin",
+    name: 'docusaurus-baidu-analytics-plugin',
     injectHtmlTags() {
       return {
         headTags: [
           {
-            tagName: "script",
+            tagName: 'script',
             innerHTML: `
             var _hmt = _hmt || [];
             (function() {
               var hm = document.createElement("script");
               hm.src = "https://hm.baidu.com/hm.js?7dda2565ba246ecd62878c13153e2941";
+              hm.defer = true;
               var s = document.getElementsByTagName("script")[0]; 
               s.parentNode.insertBefore(hm, s);
             })();
           `,
           },
           {
-            tagName: "meta",
+            tagName: 'meta',
             attributes: {
-              name: "baidu-site-verification",
-              content: "IXU12YQUjF",
+              name: 'baidu-site-verification',
+              content: 'IXU12YQUjF',
             },
           },
         ],
-      };
+      }
     },
-  };
-};
+  }
+}
