ff11603251f79c15fa9ed6083f6fb8550449ff1f
Cansiny0320 <1136843910@qq.com>
2021-06-19 14:08:59 +0800

    chore: remove deprecated config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -159,7 +159,6 @@ module.exports = {
           customCss: require.resolve("./src/css/custom.css"),
         },
         sitemap: {
-          cacheTime: 600 * 1000, // 600 sec - cache purge period
           changefreq: "daily",
           priority: 0.5,
         },
